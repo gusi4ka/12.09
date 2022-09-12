7352f89973b0e4eb287f49d48f6497815fa285a7
gusi4ka <112167073+gusi4ka@users.noreply.github.com>
2022-09-12 18:31:52 +0300

    Add files via upload

---
+++ HomeWork10.09/zada4a1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("введи число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введи второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+ double x = a, y = b;
+
+    double result = Math.Pow(x, y);
+
+    Console.WriteLine($"{x} ^ {y} = {result}");
